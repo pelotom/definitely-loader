010c28ab523ee1b54f59099f6786a119b7ebd302
Tom Crockett <pelotom@gmail.com>
2016-08-31 13:51:50 -0700

    Require `definitely` statically so it's not seen as extraneous

--- definitely-loader.js
+++ definitely-loader.js
@@ -1,3 +1,4 @@
+require('definitely'); // so it's not seen as extraneous
 module.exports = function(source) {
     this.cacheable();
     return `${source}; module.exports = require('definitely').default(module.exports, { whitelist: ["__esModule"] });`;
