93429fca4c89378caef3108ddf76fc128e5a3229
Tom Crockett <pelotom@gmail.com>
2016-08-31 14:32:23 -0700

    Do this the right way with stringifyRequest

--- src/definitely-loader.js
+++ src/definitely-loader.js
@@ -1,5 +1,11 @@
-require('definitely'); // so it's not seen as extraneous
+import { stringifyRequest } from 'loader-utils'
+
 module.exports = function(source) {
-    this.cacheable();
-    return `${source}; module.exports = require('definitely').default(module.exports, { whitelist: ["__esModule"] });`;
+    this.cacheable()
+    const definitely = stringifyRequest(this, `!!${require.resolve('definitely')}`)
+    return `
+      ${source};
+      var definitely = require(${definitely}).default;
+      module.exports = definitely(module.exports, { whitelist: ["__esModule"] });
+    `
 }
